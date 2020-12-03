5d85f32ba613bb17bcd359c9b63a2488463d1e85
Henrique Gasques <henriquegasques@gmail.com>
2020-12-03 18:54:22 -0300

    Implements order payload parsing

--- lib/order_payload_parser.ex
+++ lib/order_payload_parser.ex
@@ -8,45 +8,100 @@ defmodule OrderPayloadParser do
   """
 
   def parse(payload) do
-    %Order{
-      externalCode: Integer.to_string(payload["id"]),
-      storeId: payload["store_id"],
-      subTotal: payload["total_amount"],
-      deliveryFee: payload["LoL"],
-      total: payload["total_amount_with_shipping"],
-      country: "lol",
-      state: "SP",
-      city: "Cidade de Testes",
-      district: "Bairro Fake",
-      street: "Rua de Testes Fake",
-      complement: "galpao",
-      latitude: -23.629037,
-      longitude:  -46.712689,
-      dtOrderCreate: "2019-06-27T19:59:08.251Z",
-      postalCode: "85045020",
-      number: "0",
-      customer: %{
-        externalCode: "136226073",
-        name: "JOHN DOE",
-        email: "john@doe.com",
-        contact: "41999999999",
+    %{
+      "externalCode"   => payload["id"] |> to_str,
+      "storeId"        => payload["store_id"],
+      "subTotal"       => payload["total_amount"] |> to_str,
+      "deliveryFee"    => payload["total_shipping"] |> to_str,
+      "total_shipping" => payload["total_shipping"] |> to_str,
+      "total"          => payload["total_amount_with_shipping"] |> to_str,
+      "country"        => fetch_country_id(payload),
+      "state"          => fetch_state_name(payload),
+      "city"           => fetch_city_name(payload),
+      "district"       => fetch_district_name(payload),
+      "street"         => receiver_address(payload)["street_name"],
+      "complement"     => receiver_address(payload)["comment"],
+      "latitude"       => receiver_address(payload)["latitude"],
+      "longitude"      => receiver_address(payload)["longitude"],
+      "dtOrderCreate"  => payload["date_created"],
+      "postalCode"     => receiver_address(payload)["zip_code"],
+      "number"         => receiver_address(payload)["street_number"],
+      "customer" => %{
+        "externalCode" => get_in(payload, ["buyer", "id"]) |> to_str,
+        "name"         => get_in(payload, ["buyer", "nickname"]),
+        "email"        => get_in(payload, ["buyer", "email"]),
+        "contact"      => customer_phone_number(payload),
       },
-      items: [
-        %{
-          externalCode: "IT4801901403",
-          name: "Produto de Testes",
-          price: 49.9,
-          quantity: 1,
-          total: 49.9,
-          subItems: []
-        }
-      ],
-      payments: [
-        %{
-          type: "CREDIT_CAR",
-          value: 55.04
-        }
-      ]
+      "items"    => fetch_order_items(payload),
+      "payments" => fetch_payment_info(payload)
     }
   end
+
+  defp fetch_payment_info(payload) do
+    payload["payments"]
+    |> Enum.map(&build_payment_info/1)
+  end
+
+  defp build_payment_info(payment) do
+    %{
+      "type"  => String.upcase(payment["payment_type"]),
+      "value" => payment["total_paid_amount"]
+    }
+  end
+
+  defp fetch_order_items(payload) do
+    payload["order_items"]
+    |> Enum.map(&build_order_item/1)
+  end
+
+  defp build_order_item(item) do
+    item_quantity = item["quantity"]
+    item_price = item["unit_price"]
+
+    %{
+      "externalCode" => get_in(item, ["item", "id"]),
+      "name"         => get_in(item, ["item", "title"]),
+      "price"        => item_price,
+      "quantity"     => item_quantity,
+      "total"        => item_price * item_quantity,
+        "subItems"   => []
+    }
+  end
+
+  defp customer_phone_number(payload) do
+    phone_info = get_in(payload, ["buyer", "phone"])
+
+    "#{phone_info["area_code"]}#{phone_info["number"]}"
+  end
+
+  defp fetch_district_name(payload) do
+    payload
+    |> receiver_address
+    |> get_in(["neighborhood", "name"])
+  end
+
+  defp fetch_city_name(payload) do
+    payload
+    |> receiver_address
+    |> get_in(["city", "name"])
+  end
+
+  defp fetch_state_name(payload) do
+    payload
+    |> receiver_address
+    |> get_in(["state", "name"])
+  end
+
+  defp fetch_country_id(payload) do
+    payload
+    |> receiver_address
+    |> get_in(["country", "id"])
+  end
+
+  defp receiver_address(payload) do
+    get_in(payload, ["shipping", "receiver_address"])
+  end
+
+  defp to_str(value) when is_integer(value), do: Integer.to_string(value)
+  defp to_str(value) when is_float(value), do: :erlang.float_to_binary(value, decimals: 2)
 end
