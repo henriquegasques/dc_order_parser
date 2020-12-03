78b792d8fd006160589895c8402bc974078fa18a
Henrique Gasques <henriquegasques@gmail.com>
2020-12-03 18:52:35 -0300

    Add fixture files to use in tests

---
+++ test/order_payload_parser_test.exs
@@ -0,0 +1,28 @@
+defmodule OrderPayloadParser.OrderPayloadParserTest do
+  use ExUnit.Case, async: true
+
+  describe "parse" do
+    test "parses the payload" do
+      result = OrderPayloadParser.parse(payload())
+
+      assert result == expected_result()
+    end
+  end
+
+  defp payload do
+    filename = Path.expand("./support/payload.json", __DIR__)
+
+    read_json_fixture(filename)
+  end
+
+  defp expected_result do
+    filename = Path.expand("./support/processed_payload.json", __DIR__)
+
+    read_json_fixture(filename)
+  end
+
+  defp read_json_fixture(filename) do
+    with {:ok, body} <- File.read(filename),
+         {:ok, json} <- Jason.decode(body), do: json
+  end
+end
