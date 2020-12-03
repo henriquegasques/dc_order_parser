990cb81a2989787d9c6193803acb80e74d02b1a9
Henrique Gasques <henriquegasques@gmail.com>
2020-12-03 18:55:16 -0300

    [WIP] check validation api response before pesisting order data

--- lib/order_payload_parser_web/controllers/order_controller.ex
+++ lib/order_payload_parser_web/controllers/order_controller.ex
@@ -2,9 +2,12 @@ defmodule OrderPayloadParserWeb.OrderController do
   use OrderPayloadParserWeb, :controller
 
   def create(conn, params) do
-    parsed_order =
-      OrderPayloadParser.parse(params)
-      |> OrderPayloadParser.OrderValidationAPIClient.validate_order
+    parsed_order = OrderPayloadParser.parse(params)
+
+    case OrderPayloadParser.OrderValidationAPIClient.validate_order do
+      {:ok, %HTTPoison.Response{status_code: 200}} -> persist_order
+      {:error, %HTTPoison.Error{reason: :timeout}} -> retry
+    end
 
     render(conn, "show.json", order: parsed_order)
   end
