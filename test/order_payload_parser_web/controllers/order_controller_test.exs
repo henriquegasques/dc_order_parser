7ad40b86b6341081bd52bc39162058a35ab6a72f
Henrique Gasques <henriquegasques@gmail.com>
2020-12-04 13:14:53 -0300

    Test invalid payload responds with bad request

--- test/order_payload_parser_web/controllers/order_controller_test.exs
+++ test/order_payload_parser_web/controllers/order_controller_test.exs
@@ -1,11 +1,19 @@
 defmodule OrderPayloadParserWeb.OrderControllerTest do
   use OrderPayloadParserWeb.ConnCase
 
-  describe "create" do
-    test "creates and renders information about the created order", %{conn: conn} do
+  describe "when the parsed order is successfully validated by the validation API" do
+    test "persists the order and renders information about it", %{conn: conn} do
       conn = post(conn, Routes.order_path(conn, :create), payload())
 
       assert %{"data" => _response} = json_response(conn, 200)
+    end
+  end
+
+  describe "when the parsed order is invalidated by the validation API" do
+    test "returns the errer and does not persist the order", %{conn: conn} do
+      conn = post(conn, Routes.order_path(conn, :create), %{invalid: "payload"})
+
+      assert %{"data" => _response} = json_response(conn, 400)
     end
   end
 
