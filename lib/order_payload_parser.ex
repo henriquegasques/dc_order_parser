4fe2f709dec8e42a00bc0b47b1fea3b6df22f5d3
Henrique Gasques <henriquegasques@gmail.com>
2020-12-04 13:12:59 -0300

    Use Map.fetch! so an exception is thrown when the key is not found in payload

--- lib/order_payload_parser.ex
+++ lib/order_payload_parser.ex
@@ -9,23 +9,23 @@ defmodule OrderPayloadParser do
 
   def parse(payload) do
     %{
-      "externalCode"   => payload["id"] |> to_str,
-      "storeId"        => payload["store_id"],
-      "subTotal"       => payload["total_amount"] |> to_str,
-      "deliveryFee"    => payload["total_shipping"] |> to_str,
-      "total_shipping" => payload["total_shipping"] |> to_str,
-      "total"          => payload["total_amount_with_shipping"] |> to_str,
+      "externalCode"   => Map.fetch!(payload, "id") |> to_str,
+      "storeId"        => Map.fetch!(payload, "store_id"),
+      "subTotal"       => Map.fetch!(payload, "total_amount") |> to_str,
+      "deliveryFee"    => Map.fetch!(payload, "total_shipping") |> to_str,
+      "total_shipping" => Map.fetch!(payload, "total_shipping") |> to_str,
+      "total"          => Map.fetch!(payload, "total_amount_with_shipping") |> to_str,
       "country"        => fetch_country_id(payload),
       "state"          => fetch_state_name(payload),
       "city"           => fetch_city_name(payload),
       "district"       => fetch_district_name(payload),
-      "street"         => receiver_address(payload)["street_name"],
-      "complement"     => receiver_address(payload)["comment"],
-      "latitude"       => receiver_address(payload)["latitude"],
-      "longitude"      => receiver_address(payload)["longitude"],
-      "dtOrderCreate"  => payload["date_created"],
-      "postalCode"     => receiver_address(payload)["zip_code"],
-      "number"         => receiver_address(payload)["street_number"],
+      "street"         => Map.fetch!(receiver_address(payload), "street_name"),
+      "complement"     => Map.fetch!(receiver_address(payload), "comment"),
+      "latitude"       => Map.fetch!(receiver_address(payload), "latitude"),
+      "longitude"      => Map.fetch!(receiver_address(payload), "longitude"),
+      "dtOrderCreate"  => Map.fetch!(payload, "date_created"),
+      "postalCode"     => Map.fetch!(receiver_address(payload), "zip_code"),
+      "number"         => Map.fetch!(receiver_address(payload), "street_number"),
       "customer" => %{
         "externalCode" => get_in(payload, ["buyer", "id"]) |> to_str,
         "name"         => get_in(payload, ["buyer", "nickname"]),
@@ -38,19 +38,19 @@ defmodule OrderPayloadParser do
   end
 
   defp fetch_payment_info(payload) do
-    payload["payments"]
+    Map.fetch!(payload, "payments")
     |> Enum.map(&build_payment_info/1)
   end
 
   defp build_payment_info(payment) do
     %{
-      "type"  => String.upcase(payment["payment_type"]),
-      "value" => payment["total_paid_amount"]
+      "type"  => Map.fetch!(payment, "payment_type") |> String.upcase,
+      "value" => Map.fetch!(payment, "total_paid_amount")
     }
   end
 
   defp fetch_order_items(payload) do
-    payload["order_items"]
+    Map.fetch!(payload, "order_items")
     |> Enum.map(&build_order_item/1)
   end
 
@@ -102,6 +102,7 @@ defmodule OrderPayloadParser do
     get_in(payload, ["shipping", "receiver_address"])
   end
 
+  defp to_str(nil), do: ""
   defp to_str(value) when is_integer(value), do: Integer.to_string(value)
   defp to_str(value) when is_float(value), do: :erlang.float_to_binary(value, decimals: 2)
 end
